db0674883f7b030c987f02513d3104504432001e
kamruzzamananto000-bit <kamruzzamananto000@gmail.com>
2025-09-22 16:10:44 +0600

    Anto.c

--- delta c code.c
+++ delta c code.c
@@ -21,7 +21,26 @@
 /* ---------- Function Prototypes ---------- */
 double trapezoidal_rule(double(*f)(double),double a,double b,int n);
 double simpsons_rule(double(*f)(double),double a,double b,int n);
-/*Function to integrate*/
+//*Function to integrate*/
 
 double f1(double x) {return sin(x);}//f1=sin(x)
 double f2(double x) {return log(x);}//f2=ln(x)
+
+/* ---------- MAIN PROGRAM ---------- */
+int main(){
+  int n; //Number of subintervals
+
+//Intergration limits and exact values
+double a1 = 0.0, b1 = M_PI;   //sin(X) from 0 to π
+double a2 = 1.0, b2 = 2.0;    //ln(x) from 1 to 2
+double exact1 = 2.0;          //∫0 to π sin(x) dx
+double exact2 = 2 * log(2) - 1;   // ∫1 to 2 ln(x) dx
+
+printf("Enter numbers of intervals (n): ");
+scanf("%d", &n);
+
+// Simpson's Rule requires even numbers of subintervals
+if(n % 2 != 0){
+   printf("Simpson's Rule requires even n, so using n+1.\n");
+   n++;
+}
