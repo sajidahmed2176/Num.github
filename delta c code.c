0aa36550d84b08758d1359d0fd023eee9998a5a9
shariful islam <2024000000235@seu.edu.bd>
2025-09-23 07:32:16 +0600

    sharif.c

--- delta c code.c
+++ delta c code.c
@@ -103,7 +103,7 @@ double sum = f(a) + f(b);
 for(int i = 1; i < n; i++){
     if (i % 2 == 0)
       sum += 2 * f (a + i * h);
-   else 
+       else 
       sum += 4 * f (a + i * h);
 }
 return (h /3.0) * sum;
