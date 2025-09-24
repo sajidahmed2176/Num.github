52871f2ca580d87996747b79f8aa3d672906e8c6
shariful islam <2024000000235@seu.edu.bd>
2025-09-24 14:20:06 +0600

    Update delta c code.c

--- delta c code.c
+++ delta c code.c
@@ -1,15 +1,11 @@
 #include<stdio.h>
 #include<math.h>
 /*
+
  * CSE261 Group Assignment
  * Topic: Implementation and Comparison of Simpson's and Trapezoidal Rules
  * Language: C
- *
- * --- How to Compile and Run ---
- * 1. Save the code as 'integration_comparison.c'
- * 2. Compile: gcc integration_comparison.c -o integration_comparison -lm
- * 3. Run: ./integration_comparison
- *
+ 
  * This program calculates the definite integrals of:
  *   (1) sin(x) from 0 to π
  *   (2) ln(x)  from 1 to 2
