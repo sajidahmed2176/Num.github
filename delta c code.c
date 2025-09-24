1d2010457b51d5ee2cd7ccc8601060f4d7d78a2d
shariful islam <2024000000235@seu.edu.bd>
2025-09-24 14:26:13 +0600

    Update delta c code.c

--- delta c code.c
+++ delta c code.c
@@ -43,20 +43,23 @@ if(n % 2 != 0){
    n++;
 }
 
-/* ---Compute integrals using both methods ---*/
+// Compute integrals using both methods 
+ 
 double trap1 =trapezoidal_rule(f1,a1,b1,n);
 double simp1 =simpsons_rule(f1,a1,b1,n);
 
 double trap2 =trapezoidal_rule(f2,a2,b2,n);
 double simp2 =simpsons_rule(f2,a2,b2,n);
 
-/* --- Calculate percentage errors ---*/
+// Calculate percentage errors 
+ 
 double error_trap1 =fabs(trap1 - exact1) / exact1 * 100.0;
 double error_simp1 =fabs(simp1 - exact1) / exact1 * 100.0;
 double error_trap2 =fabs(trap2-exact2) / exact2 * 100.0;
 double error_simp2 =fabs(simp2 -exact2) / exact2 * 100.0;
 
-/* --- Display results --- */
+// Display results
+ 
     printf("\n--- Numerical Integration Results ---\n");
 
     printf("\n1) Integral of sin(x) from 0 to π:\n");
